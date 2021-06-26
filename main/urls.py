f21a9645f03999fc8054e14dcd8250d7f59c4427
Israel Sagesse <israelsagesse@gmail.com>
2021-06-26 07:41:52 -0400

    initial commit

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('create', views.create),
     path('result', views.result),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 
+def create(request):
+    request.session['user_name'] = request.POST['user_name']
+    request.session['location'] = request.POST['location']
+    request.session['language'] = request.POST['language']
+    request.session['comment'] = request.POST['comment']
+    
+    return redirect("/")
+
 def result(request):
     context = {
-        'name': request.POST['user_name'],
-        'location': request.POST['location'],
-        'language': request.POST['language'],
-        'comment': request.POST['comment']
+        'name': request.session['user_name'],
+        'location': request.session['location'],
+        'language': request.session['language'],
+        'comment': request.session['comment']
     }
     return render(request, 'result.html', context)
